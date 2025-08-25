86cf6589c3049b209259af69e7a4fed066e63d94
Dundappa <90092572+yadappagol@users.noreply.github.com>
2025-08-25 22:01:43 +0530

    Create thirdProgrammFile.py

---
+++ thirdProgrammFile.py
@@ -0,0 +1,18 @@
+This is python for loop concept information
+
+Syntax:
+
+for variable in sequence:
+    # code block
+
+program:
+fruits = ["apple", "banana", "cherry"]
+
+for fruit in fruits:
+    print(fruit)
+
+output:
+
+apple
+banana
+cherry
