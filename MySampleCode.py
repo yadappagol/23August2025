12ebd7a7348d7a747a67f6c6d2d70732d61254d8
dundappa <dbydby56@gmail.com>
2025-08-25 21:47:28 +0530

    My Second Sample Code File

---
+++ MySampleCode.py
@@ -0,0 +1,16 @@
+
+
+This is my Python Sample Code
+
+
+# Program to add two numbers using fixed values
+
+num1 = 10
+num2 = 5
+
+# Add the numbers
+sum = num1 + num2
+
+# Display the result
+print("The sum is:", sum)
+
